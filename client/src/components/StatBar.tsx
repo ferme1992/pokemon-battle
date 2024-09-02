3213cf4eff1269805eb5d6311703e1b9b414573f
Fernando Estevez <fernando.estevez@patagoniansys.com>
2024-09-02 10:18:56 -0300

    Added Pokemon stat bar component

---
+++ client/src/components/StatBar.tsx
@@ -0,0 +1,22 @@
+import { Box, LinearProgress, Typography } from "@mui/material";
+import { FC } from "react";
+
+interface StatBarProps {
+  label: string;
+  value: number;
+}
+
+const StatBar: FC<StatBarProps> = ({ label, value }) => (
+  <Box display="flex" alignItems="center" mb={1}>
+    <Typography variant="body2" minWidth={60}>
+      {label}
+    </Typography>
+    <LinearProgress
+      variant="determinate"
+      value={value * 20}
+      sx={{ flexGrow: 1, height: 10, borderRadius: 5 }}
+    />
+  </Box>
+);
+
+export default StatBar;
