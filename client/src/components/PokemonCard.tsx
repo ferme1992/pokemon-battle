4d6f94f67b7bf79a619c7755e77619d830c4b089
Fernando Estevez <fernando.estevez@patagoniansys.com>
2024-09-02 10:20:22 -0300

    Added Pokemon Type and Card

---
+++ client/src/components/PokemonCard.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Pokemon } from "../types/Pokemon";
+
+interface PokemonCardProps {
+  pokemon: Pokemon;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const PokemonCard: FC<PokemonCardProps> = ({
+  pokemon,
+  isSelected,
+  onClick,
+}) => {
+  return (
+    <Card
+      onClick={onClick}
+      sx={{
+        cursor: "pointer",
+        border: isSelected ? "2px solid blue" : "none",
+        "&:hover": { boxShadow: 3 },
+      }}
+    >
+      <CardContent>
+        <img
+          src={pokemon.imageUrl}
+          alt={pokemon.name}
+          style={{ width: "100%", height: "auto" }}
+        />
+        <Typography variant="subtitle1" align="center">
+          {pokemon.name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PokemonCard;
---
+++ client/src/types/Pokemon.ts
@@ -0,0 +1,11 @@
+export interface Pokemon {
+    id: string;
+    name: string;
+    attack: number;
+    defense: number;
+    hp: number;
+    speed: number;
+    type: string;
+    imageUrl: string;
+  }
+  
