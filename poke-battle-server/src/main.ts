9ea6b158e41bf1488bcf23086556ab8323514d32
Fernando Estevez <fernando.estevez@patagoniansys.com>
2024-09-02 16:43:33 -0300

    Enabled CORS

--- poke-battle-server/src/main.ts
+++ poke-battle-server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: false,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
